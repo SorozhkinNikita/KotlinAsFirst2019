ec53133f3722b37e0552617a76270285f9428a0b
sorozhkin.na@edu.spbstu.ru <Snikitaa22992200>
2019-12-25 05:35:33 +0300

    7

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -217,7 +217,15 @@ fun alignFileByWidth(inputName: String, outputName: String) {
  * Ключи в ассоциативном массиве должны быть в нижнем регистре.
  *
  */
-fun top20Words(inputName: String): Map<String, Int> = TODO()
+fun top20Words(inputName: String): Map<String, Int> {
+    val res = mutableMapOf<String, Int>()
+    for (line in File(inputName).readLines()) {
+        val words = Regex("""[^a-zA-zа-яА-ЯёЁ]+""").split(line).map { it.toLowerCase() }.filter { it.isNotEmpty() }
+        for (i in words) res[i] = res.getOrDefault(i, 0) + 1
+    }
+    return res.toList().sortedByDescending { it.second }.take(20).toMap()
+}
+
 
 /**
  * Средняя
